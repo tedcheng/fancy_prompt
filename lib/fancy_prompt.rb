b60977a9a833e3da2cbefc66f5babb0fb1e59589
tedcheng <yiqun.cheng@gmail.com>
2017-04-05 23:42:57 -0700

    add env code for pry

--- lib/fancy_prompt.rb
+++ lib/fancy_prompt.rb
@@ -1,5 +1,14 @@
 require "fancy_prompt/version"
 
 module FancyPrompt
-  # Your code goes here...
+
 end
+
+if defined?(Rails::Console) && defined?(Pry)
+  Pry.config.prompt = Pry::DEFAULT_PROMPT
+  env = Rails.env.to_s
+  env = "dev" if env == 'development'
+  env = "prod" if env == 'production'
+  Pry.config.prompt_name = "#{Rails.application.class.parent_name.downcase}|#{env}"
+end
+
